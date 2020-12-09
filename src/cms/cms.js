23869c03e74d804049a51c81e6d46062028d57d4
Matthew Souther <matthewsouther@gmail.com>
2020-12-08 19:52:51 -0800

    Add services and resources pages

--- src/cms/cms.js
+++ src/cms/cms.js
@@ -5,6 +5,8 @@ import cloudinary from 'netlify-cms-media-library-cloudinary'
 import AboutPagePreview from './preview-templates/AboutPagePreview'
 import BlogPostPreview from './preview-templates/BlogPostPreview'
 import IndexPagePreview from './preview-templates/IndexPagePreview'
+import ResourcesPagePreview from './preview-templates/ResourcesPagePreview'
+import ServicesPagePreview from './preview-templates/ServicesPagePreview'
 
 CMS.registerMediaLibrary(uploadcare)
 CMS.registerMediaLibrary(cloudinary)
@@ -12,3 +14,5 @@ CMS.registerMediaLibrary(cloudinary)
 CMS.registerPreviewTemplate('index', IndexPagePreview)
 CMS.registerPreviewTemplate('about', AboutPagePreview)
 CMS.registerPreviewTemplate('blog', BlogPostPreview)
+CMS.registerPreviewTemplate('resources', ResourcesPagePreview)
+CMS.registerPreviewTemplate('services', ServicesPagePreview)
